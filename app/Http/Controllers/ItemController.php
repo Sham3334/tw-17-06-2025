4ca9ae4ec462b338d78c68ff6428e76ebef179c2
Sham3334 <Sham33343335@gmail.com>
2025-06-17 17:37:54 +0300

    feat: Добавил ItemController, ItemResource index маршрут

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ItemResource;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        $items = Item::all();
+        return ItemResource::collection($items);
+    }
+}
